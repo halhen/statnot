3b0835f03176af9158397aa87e20555522e55655
Henrik Hallberg <halhen@k2h.se>
2009-04-14 00:00:52 +0200

    External script is now passed to the notification for formatting. Credit for idea: BetterLeftUnsaid @ bbs.archlinux.org

--- .statusline.sh
+++ .statusline.sh
@@ -1,4 +1,7 @@
-loadavg="`cat /proc/loadavg | awk '{print $1, $2, $3}'`"
-
-echo "[load ${loadavg}]   `date +'%R'`"
+if [ $# -eq 0 ]; then
+    loadavg="`cat /proc/loadavg | awk '{print $1, $2, $3}'`";
+    echo "[load ${loadavg}]   `date +'%R'`";
+else
+    echo "NOTIFICATION: $1";
+fi
 
